7e0c331da42aace517c10faf32f6cf3d48a502d1
Joel Ranjith Jebanesan <joel@tivonaglobal.com>
2019-12-02 18:12:16 +0530

    reverse pyramid functions incomplete commit

--- reverse_pyramid.py
+++ reverse_pyramid.py
@@ -1,9 +1,12 @@
 def reverse_pyramid(n):
-    for i in range(0,n):
-        m = 2*n
-        space = " "*(m-1)
-        rev_py = space + "*"
-
-    print(rev_py)
+    for i in range(1,n):
+        myList = []
+        for j in range(1,i+2):
+            m = 2*n
+            space = " "*(m-1)
+            rev_py = (space + "* ") * j
+            m = m-2
+            myList.append(rev_py)
+    print ("\n".join(myList))
 
 reverse_pyramid(5)
